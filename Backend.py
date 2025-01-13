5a69e0008ab091f563cb753339a5039c66cb8f46
danibo1117 <danibo900@gmail.com>
2025-01-13 20:25:06 +0100

    uploaded docx and fixed backend file name

--- Backend.py
+++ Backend.py
@@ -7,11 +7,11 @@ def saveToJson(pacienti):
         novy_pacienti.append(pacient.toDict())
     
     final_dict = {"Pacienti": novy_pacienti}
-    json.dump(final_dict, open("Semestrálna práca//pacientdata.json", "w"), indent=6)
+    json.dump(final_dict, open("pacientdata.json", "w"), indent=6)
 
 def loadFromJson():
     pacienti = []
-    final_dict = json.load(open("Semestrálna práca//pacientdata.json","r"))
+    final_dict = json.load(open("pacientdata.json","r"))
     novy_pacienti = final_dict["Pacienti"]
     for pacient in novy_pacienti:
         pacient_objekt = Pacient(pacient["id"], pacient["meno"],pacient["priezvisko"],pacient["datumnarod"], pacient["posledna_navs"],pacient["diagnoza"], pacient["predpis_liek"])
